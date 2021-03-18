bc377b5e1784409fb53796120230580c8ec3cf01
Chayaphon256 <pium.vipant@gmail.com>
2021-03-19 04:14:31 +0700

    up

--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ import firebase from 'firebase/app'
 import "firebase/firestore";
 import 'firebase/auth';
 if (!firebase.apps.length) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyDJ8R7ofZr-_fOk8mjwmVmRVdrgbqSy8is",
-    authDomain: "midori-project007.firebaseapp.com",
-    projectId: "midori-project007",
-    storageBucket: "midori-project007.appspot.com",
-    messagingSenderId: "676030659556",
-    appId: "1:676030659556:web:dec16b7a8e489e307665f1",
+  var firebaseConfig = {
+    apiKey: "AIzaSyDvept7ziud4UC3eF3rQKJ2DZg4FwVxMxE",
+    authDomain: "midorilogin.firebaseapp.com",
+    projectId: "midorilogin",
+    storageBucket: "midorilogin.appspot.com",
+    messagingSenderId: "823495147603",
+    appId: "1:823495147603:web:8927b4d983e7e35097c6a6",
+    measurementId: "G-NE4WJ3X7GM"
   };
   firebase.initializeApp(firebaseConfig)
 }
